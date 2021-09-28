18986527157d80828f8594ccd73c40eaa7fcf2e8
Dick Ström <dick5strom@gmail.com>
2021-09-28 08:51:54 +0200

    Initial commit

---
+++ home.js
@@ -0,0 +1,80 @@
+//console.log("hello");
+//alert("Hi this is Dick");
+//var b = "smothie";
+//console.log(b);
+//var someNumber = 45;
+//console.log(someNumber);
+//function What() {
+   // document.getElementById('text').innerHTML= "Noo..really..thats..old";
+   // prompt('How old are you?');
+//}
+//var num1 = 10;
+//num1++;
+//console.log(num1);
+//num1--;
+//console.log(num1);
+
+//function fun(){
+   // console.log('This is a function.')
+
+//}
+//fun();
+
+
+//function greeting(){
+   // var name = prompt('What is your name');
+   // var result ='Hello my dear ' + name;
+    //console.log(result);
+//}
+//greeting();
+
+//function sumNumbers(num1, num2){
+    //var result = num1 + num2;
+   // console.log(result);
+//}
+//sumNumbers(20, 30);
+/* while loop
+var num = 0;
+while (num<100){
+num += 1;
+console.log(num);
+}
+*/
+//for loop
+//for (let num = 0; num < 100; num++){
+    //console.log(num);
+//}
+//Data types
+var yourAge = 18; //number
+let first = 'Susan';
+let last = ' The Lobster'; //string
+let yourName = first + ' ' + last;//objekt
+let truth = false;//bolean
+let groceries = ['apple', 'banana', 'oranges'];//array
+let random;//undefined
+let nothing = null;//value null
+console.log(yourName);
+
+//strings in JS
+let fruit = 'banana,apple,melon,blueberry';
+let moreFruits = 'banana/apple'; //new line
+
+console.log(fruit.length);
+console.log(fruit.indexOf('nan'));
+console.log(fruit.slice(2, 6));
+console.log(fruit.replace('ban', 'Tob'));
+console.log(fruit.toUpperCase ());
+console.log(fruit.toLowerCase ());
+console.log(fruit.charAt(4));
+console.log(fruit[3]);
+console.log(fruit.split(',')); //split by a comma
+console.log(fruit.split('')); //split by charactar
+
+//Array
+let fruits = ['banana', 'apple', 'orange', 'pinapples'];
+fruits = new Array('banana', 'apple', 'orange', 'pinapples');
+console.log ( fruits[2])
+fruits[0] ='pear';
+console.log(fruits);
+fruits[2] ='bananahammock';
+console.log(fruits);
