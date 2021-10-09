62cf27194158b1d7dfbd2ec12b1ec8c391e4842d
dickstrom <83727995+dickstrom@users.noreply.github.com>
2021-10-09 20:02:13 +0200

    worked to the console

--- home.js
+++ home.js
@@ -1,13 +1,8 @@
+
+
 //console.log("hello");
-//alert("Hi this is Dick");
-//var b = "smothie";
-//console.log(b);
-//var someNumber = 45;
-//console.log(someNumber);
-//function What() {
-   // document.getElementById('text').innerHTML= "Noo..really..thats..old";
-   // prompt('How old are you?');
-//}
+
+
 //var num1 = 10;
 //num1++;
 //console.log(num1);
@@ -48,7 +43,7 @@ console.log(num);
 var yourAge = 18; //number
 let first = 'Susan';
 let last = ' The Lobster'; //string
-let yourName = first + ' ' + last;//objekt
+let yourName = first + ' ' + last;//object
 let truth = false;//bolean
 let groceries = ['apple', 'banana', 'oranges'];//array
 let random;//undefined
@@ -71,10 +66,94 @@ console.log(fruit.split(',')); //split by a comma
 console.log(fruit.split('')); //split by charactar
 
 //Array
-let fruits = ['banana', 'apple', 'orange', 'pinapples'];
-fruits = new Array('banana', 'apple', 'orange', 'pinapples');
-console.log ( fruits[2])
-fruits[0] ='pear';
-console.log(fruits);
-fruits[2] ='bananahammock';
-console.log(fruits);
+let fruits = ['banana', 'apple', 'orange', 'pinapples',];
+fruits = new Array('banana', 'apple', 'orange', 'pinapples' , 'wonderfruits');
+//console.log ( fruits[2])
+//fruits[0] ='pear';
+//console.log(fruits);
+//fruits[2] ='bananahammock';
+//console.log(fruits);
+//console.log(fruits[4]);
+
+for (let i = 0; i < fruits.length; i++) {
+   console.log(fruits [i] );
+}
+console.log('to string' ,fruits.toString());
+console.log(fruits.join('*'));
+console.log(fruits.pop(), fruits);
+console.log(fruits.push('blackberries'), fruits);
+fruits.shift(); 
+ 
+let vegetables = ['tomatoes', 'qucumber', 'carrots'];
+let allGroceries = fruits.concat(vegetables);
+console.log(allGroceries);
+console.log(allGroceries.reverse());
+console.log(allGroceries.slice(1,4));
+console.log(allGroceries.sort());
+
+let someNumber= [1, 4, 67, 45,34,23,89,99, 144,544];
+console.log(someNumber.sort(function(a,b){return a-b}));
+console.log(someNumber.sort(function(a,b){return b-a}));
+
+let emptyArray = new Array();
+for (let num = 0; num < 10; num++){
+   emptyArray.push(num);
+}
+console.log(emptyArray);
+   
+   let student = {
+   first:'Dick', 
+   last: 'Strom',
+   age:52,
+   height:177,
+   studentInfo: function(){
+      return this.first + '\n' + this.last + '\n' + this.age
+   }
+
+};
+console.log(student.first);
+console.log(student.last);
+student.first = 'Big Dick';
+console.log(student.first);
+student.age++;
+console.log(student.age);
+console.log(student.studentInfo());
+
+//Conditionals, control flows(if else)
+//var age = prompt('What is yor  age?')
+//if( (age >= 18) && (age <= 35)){
+  // var status = prompt('Target audience')
+//}else{
+  // var status = prompt('Not my audience')
+//}
+
+//switch statements
+//difference between weekend or weekday
+//day 0 sunday--weekend
+//day 1 monday
+//day 2 tuesday
+//day 3 wednesday
+//day 4 thursday
+//day 5 friday
+//day 6 saturday--weekend
+
+switch(6) {
+   case 0:
+      text = 'weekend';
+      break;
+   case 5:
+      text = 'weekend';
+      break;
+   case 6:
+      text = 'weekend';
+      break;
+      default:
+      text = 'weekday';   
+}
+console.log(text);
+
+
+
+
+
+
